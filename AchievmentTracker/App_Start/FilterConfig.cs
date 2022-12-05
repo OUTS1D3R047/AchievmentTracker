8894ce802134d7136fd1d44bee5c76b64b4da142
aizek <aizek@DESKTOP-GV27N2V>
2022-12-05 11:32:28 +0300

    Добавьте файлы проекта.

---
+++ AchievmentTracker/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AchievmentTracker
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
